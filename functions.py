75ec03ae9de5a7e39ec1f9dc63ced11803e8bc7d
Tasha-Tech <tasha.tech04@gmail.com>
2020-05-21 04:09:00 +0000

    Our first lesson

--- functions.py
+++ functions.py
@@ -1,1 +1,17 @@
 print("Hello to functions lesson")
+
+def calc1(a, b):
+  c = a * a + b
+  return c
+
+x = calc1(4, 6)
+print("The result")
+print(x)
+print("The result " + str(x) ) 
+
+def calc2(a, b, c):
+  summary = a + b + c
+  return summary
+
+Q = calc2(5, 3, 33)
+print("Total " + str(Q) )
