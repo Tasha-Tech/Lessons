c38c90687940135745b54388acdeeb0bbc6c0dbc
Tasha-Tech <tasha.tech04@gmail.com>
2020-05-21 04:27:36 +0000

    Homework

--- functions.py
+++ functions.py
@@ -15,3 +15,13 @@ def calc2(a, b, c):
 
 Q = calc2(5, 3, 33)
 print("Total " + str(Q) )
+
+# Homework !!!
+# Define function named calc3
+# This function will receive one argument
+# it will calculate qubic value of this argument
+# and also this function will print argument and it's qubic value
+# Call this function two times.
+# Result should look like:
+# Value 5 in power of 3 is 125
+# Value 3 in power of 3 is 27
