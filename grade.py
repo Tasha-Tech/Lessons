60c4a1b5be961e823695fa0244bb620789c648d0
null null <tasha.tech04@gmail.com>
2020-05-23 23:36:50 +0000

    grade3

--- grade.py
+++ grade.py
@@ -45,5 +45,35 @@ print("Your grade is " + Z)
 Z = grade2(88)
 print("Your grade is " + Z)
 
-Z = grade2(92)
-print("Your grade is " + Z)
+# Z = grade2(92)
+print("Your grade is " + grade2(92))
+
+def grade3(name, grade):
+  if grade > 90:
+    print(name  + " " + str(grade)  + " A")
+    return "A"
+
+  if grade > 80:
+    print(name  + " " + str(grade)  + " B")
+    return "B"
+
+  if grade > 70:
+    print(name  + " " + str(grade)  + " C")
+    return "C"
+
+  if grade > 60: 
+    print(name  + " " + str(grade)  + " D")
+    return "D"
+
+  print(name  + " " + str(grade)  + " F")
+  return "F"
+
+y = grade3('Honey pie', 82)
+print("Your grade is " + y)
+
+y = grade3('youYouy', 36)
+print("Your grade is " + y)
+
+s = 'XYZ'
+y = grade3(s, 87)
+print("Your grade is " + y)
