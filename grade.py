a15ea265a403a8beeb84ba8aed86808509bd2b1c
null null <tasha.tech04@gmail.com>
2020-05-23 22:41:18 +0000

    grade1

--- grade.py
+++ grade.py
@@ -1,1 +1,25 @@
 print("Welcome to a condition lesson!")
+
+def grade1(x):
+  if x > 90:
+    return "A"
+
+  if x > 80:
+    return "B"
+
+  if x > 70:
+    return "C"
+
+  if x > 60: 
+    return "D"
+
+  return "F"
+
+y = grade1(95)
+print("Your grade is " + y)
+
+y = grade1(36)
+print("Your grade is " + y)
+
+y = grade1(87)
+print("Your grade is " + y)
