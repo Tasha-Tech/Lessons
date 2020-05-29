b8379a19edfa76a7af12615b7edcd07e6232b3db
TashaTech <tasha.tech04@gmail.com>
2020-05-29 02:53:33 +0000

    Homework (credits)

--- credit.py
+++ credit.py
@@ -1,2 +1,24 @@
 print("Wellcome to credit homework!")
 
+'''
+1. Create function credit1
+This function should have two arguments:
+name: str - student name
+grade: int - grade received by student
+function will return string value "PASS" or "FAIL"
+If grade is gtaer than 75, function shell returm "PASS",
+otherwise "FAIL" shell be returned.
+Write couple tests for this function.
+
+2. Create function credit2
+This function should have three arguments:
+name: str - student name
+grade: int - grade received by student
+threshould: int - threshould for "PASS"
+function will return string value "PASS" or "FAIL"
+If grade is gtaer than threshould, function shell returm "PASS",
+otherwise "FAIL" shell be returned.
+Write couple tests for this function.
+ 
+
+'''
